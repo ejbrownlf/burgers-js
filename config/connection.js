31a70e752c119cbe42c1aaf81c7cfa859fb681d1
Erik Brown <ejbrownlf@gmail.com>
2021-03-10 15:18:51 -0600

    JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // NOTE: Be sure to add your MySQL password here!
-  password: '',
-  database: 'burgers_db',
-});
+const JAWSDB_URL = 'mysql://vf0pg47r1xtek3ei:g43faq4gj8bjevhy@pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nd4zgth1wzo7aj71';
+
+let connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // NOTE: Be sure to add your MySQL password here!
+    password: '',
+    database: 'burgers_db',
+  });
+}
 
 // Make connection.
 connection.connect((err) => {
